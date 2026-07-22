b905a8bdeb5bde881fe1ac73318f61f7ac9a830a
hhk <3067400680@qq.com>
2026-07-22 22:42:03 +0800

    完成用户模块开发 - 新增 UserController、UserService、UserMapper - 新增用户登录/注册/查询/更新 DTO 和 VO - 新增 AuthCheck 注解和登录拦截器 - 配置 MyBatis 代码生成器

--- src/main/java/com/hhk/aicoder/AiCoderApplication.java
+++ src/main/java/com/hhk/aicoder/AiCoderApplication.java
@@ -1,9 +1,11 @@
 package com.hhk.aicoder;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.hhk.aicoder.mapper")
 public class AiCoderApplication {
 
     public static void main(String[] args) {
